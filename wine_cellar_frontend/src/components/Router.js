62b4d7120aeafb745396e9501db9b713389d2afa
Maddog164 <denise@henderson3.us>
2021-08-27 17:18:42 -0400

    Trying to consolidate routing

--- wine_cellar_frontend/src/components/Router.js
+++ wine_cellar_frontend/src/components/Router.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Switch, Route, Redirect} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {NavBar} from './Navbar'
@@ -9,44 +9,50 @@ import UsersList from './UsersList'
 import UserContainer from './UserContainer'
 import WineContainer from './WineContainer'
 import UserWinesContainer from './UserWinesContainer'
+import {getAllUsers} from '../actions/userActions'
+import {getAllWines} from '../actions/wineActions'
 // import About from '../components/About'
 
-const Router = (store) => {
-    const state = store.getState()
+class Router extends Component {
 
+    componentDidMount() {
+        this.props.getAllUsers()
+        this.props.getAllWines()
+    };
 
-const Routes = props => (
-    // return (
-    <Router>
-        <Switch>
-            <Route exact path="/Home" component={Home}/>
-            <Route exact path = "/">
-                <Redirect to="/Home" />
-            </Route>
-            <Route exact path="/About" component={About} />
-            <Route
-                path="/users"
-                users={this.props.users}
-                wines={this.props.wines}
-                component = {UserContainer} 
-            />
+// const Routes = props => (
+    render() {
+        return (
+        <Router>
+            <Switch>
+                <Route exact path="/Home" component={Home}/>
+                <Route exact path = "/">
+                    <Redirect to="/Home" />
+                </Route>
+                <Route exact path="/About" component={About} />
+                <Route
+                    path="/users"
+                    users={this.props.users}
+                    wines={this.props.wines}
+                    component = {UserContainer} 
+                />
 
-            <Route path = "/wines" component = {WineContainer} />
-            <Route path = "/users/:id" component = {UserWinesContainer} />
-        </Switch>
-    </Router>
-    )
-    
-
-    const mapStateToProps = state => {
-        return {
-            users: state.userReducer.users,
-            wines: state.wineReducer.wines}
+                <Route path = "/wines" component = {WineContainer} />
+                <Route path = "/users/:id" component = {UserWinesContainer} />
+            </Switch>
+        </Router>
+        )
     }
+}
+
+const mapStateToProps = state => {
+    return {
+        users: state.userReducer.users,
+        wines: state.wineReducer.wines}
+};
 
     // const mapStateToProps = state => {
     //     return {users: state.userReducer.users}
     // }
-}
 
-export default connect(mapStateToProps)(Routes)
+export default connect(mapStateToProps)(Router)
